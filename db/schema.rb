4e259e45b1524003b68fb3c7fca333e68cc3d8a7
SPrio <mahantasupriyo@gmail.com>
2021-01-20 17:58:21 +0530

    added image to instruments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_114224) do
+ActiveRecord::Schema.define(version: 2021_03_06_122646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_114224) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
