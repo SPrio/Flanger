d8bf08481527f21879496539881b2969df67832f
SPrio <mahantasupriyo@gmail.com>
2021-03-06 19:51:08 +0530

    Fix typos

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,7 @@
 class Instrument < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON # If you use SQLite, add this line
-  belongs_to :user, optional: True
+  belongs_to :user, optional: true
 
   validates :title, :brand, :price, :model, presence: true
   validates :description, length: { maximum: 1000, too_long: '%<count>s characters is the maximum allowed. ' }
