5c141fd3b0b2f7d661654ddd8f49e41e6281e313
SPrio <mahantasupriyo@gmail.com>
2021-03-06 17:40:22 +0530

    added instruments as index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root to: 'store#index'
+  root to: 'instruments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
