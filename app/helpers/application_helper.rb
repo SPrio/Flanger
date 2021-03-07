04b52cb5402cc51415e09c03b57c44f934a07b96
SPrio <mahantasupriyo@gmail.com>
2021-03-07 13:02:26 +0530

    added application helper methods for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cart_count_over_one
+    "<span class='tag is-dark'>#{@cart.line_items.count}</span>".html_safe if @cart.line_items.count.positive?
+  end
+
+  def cart_has_items
+    @cart.line_items.count
+  end
 end
